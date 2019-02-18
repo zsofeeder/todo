161be486a0daddee9db685c2c56dfdfd6c57f01c
zsofeeder <syntaxyyy@gmail.com>
2019-02-18 08:31:12 +0100

    added routing

--- frontend/todo/src/app/app-routing.module.ts
+++ frontend/todo/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {WelcomeComponent} from './welcome/welcome.component';
+import {ErrorComponent} from './error/error.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'welcome', component: WelcomeComponent },
+  { path: '**', component:  ErrorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
